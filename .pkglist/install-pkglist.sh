9b7e15103dd8602d110451ca0d9e72164e518bf7
Mlakar Alexandru Laurian <alexandrumlakar@gmail.com>
2024-05-01 22:59:00 +0300

    Update install-pkglist script

--- .pkglist/install-pkglist.sh
+++ .pkglist/install-pkglist.sh
@@ -3,8 +3,8 @@
 # shellcheck disable=SC2024
 sudo pacman -S --needed - <pkglist-repo.txt
 
-if command -v paru -S --skipreview &>/dev/null; then
-  for x in $(<pkglist-aur.txt); do paru -S --skipreview "$x"; done
+if command -v paru -S --noconfirm --skipreview &>/dev/null; then
+  for x in $(<pkglist-aur.txt); do paru -S --noconfirm --skipreview "$x"; done
 else
   if command -v yay -S --noconfirm --mflags --skipinteg &>/dev/null; then
     for x in $(<pkglist-aur.txt); do yay -S --noconfirm --mflags --skipinteg "$x"; done
