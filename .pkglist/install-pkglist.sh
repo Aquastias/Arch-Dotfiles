52e13aa2993e3f3f191157ba69bce110f7e0578f
Mlakar Alexandru Laurian <alexandrumlakar@gmail.com>
2024-05-01 22:37:48 +0300

    Update install-pkglist script

--- .pkglist/install-pkglist.sh
+++ .pkglist/install-pkglist.sh
@@ -4,10 +4,10 @@
 sudo pacman -S --needed - <pkglist-repo.txt
 
 if command -v paru -S --skipreview &>/dev/null; then
-  for x in $(<pkglist-aur.txt); do paru -S "$x"; done
+  for x in $(<pkglist-aur.txt); do paru -S --skipreview "$x"; done
 else
   if command -v yay -S --noconfirm --mflags --skipinteg &>/dev/null; then
-    for x in $(<pkglist-aur.txt); do yay -S "$x"; done
+    for x in $(<pkglist-aur.txt); do yay -S --noconfirm --mflags --skipinteg "$x"; done
   else
     echo "Please install paru or yay."
   fi
