377bfaf74d768d33069eba72ff7a373274f56dce
Mlakar Alexandru Laurian <alexandrumlakar@gmail.com>
2024-05-07 23:25:11 +0300

    Update enable chaotic AUR script

--- custom-scripts/enable-chaotic-aur.sh
+++ custom-scripts/enable-chaotic-aur.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-# Check if running as root
-if [[ $(id -u) -ne 0 ]]; then
-    echo "This script must be run as root" 
-    exit 1
-fi
+# shellcheck source=/dev/null
+source "$SHELL_COMMONS/permissions.sh"
+
+check_root
 
 # Install primary key, keyring and mirrorlist
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 # Check if the pacman.conf already contains the [chaotic-aur] repository
 if grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
-	echo "pacman.conf already contains the [chaotic-aur] repository configuration."
-	exit 0
+  echo "pacman.conf already contains the [chaotic-aur] repository configuration."
+  exit 0
 fi
 
 # Backup original pacman.conf
@@ -32,5 +31,5 @@ current_pacman_conf=$(cat /etc/pacman.conf)
 updated_pacman_conf="$current_pacman_conf\n$new_repo_config"
 
 # Write the updated pacman.conf
-echo -e "$updated_pacman_conf" > /etc/pacman.conf
+echo -e "$updated_pacman_conf" >/etc/pacman.conf
 echo "New pacman.conf written."
