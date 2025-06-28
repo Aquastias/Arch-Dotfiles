69ec1291315962e5617636a0691f45ca78b58a18
Mlakar Alexandru <alexandrumlakar@protonmail.com>
2025-06-28 23:51:35 +0300

    chore(pkglist): Separate install/save scripts for different hosts

---
+++ .pkglist/hosts/legion/install-pkglist.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# shellcheck source=/dev/null
+source "$SHELL_COMMONS/arrays.sh"
+source "$SHELL_COMMONS/commands.sh"
+source "$SHELL_COMMONS/packages.sh"
+source "$SHELL_COMMONS/permissions.sh"
+
+check_root
+check_command "paru"
+
+ignore_pkgs=("teamspeak3")
+
+# Install packages from repository
+# shellcheck disable=SC2024
+echo "Installing repo packages..."
+pacman -S --needed - <pkglist-repo.txt
+
+# Install packages from AUR
+echo "Installing AUR packages..."
+
+for pkg in $(<pkglist-aur.txt); do
+  if ! package_installed "$pkg" && ! array_contains "$pkg" "${ignore_pkgs[@]}"; then
+    paru -S --noconfirm --skipreview "$pkg"
+  else
+    echo "$pkg is already installed."
+  fi
+done
+
+# Execute teamspeak3 script
+chmod +x ./programs/teamspeak3/install.sh && ./programs/teamspeak3/install.sh
+
+# Execute SearxNG scripts
+chmod +x ./programs/searxng/install.sh && ./programs/searxng/install.sh
+chmod +x ./programs/searxng/update.sh && ./programs/searxng/update.sh
+
+echo "All packages now installed!"
