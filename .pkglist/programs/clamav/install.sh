521cd6cb27171ef42fe56450e96bf2ca2dc8ae0e
Mlakar Alexandru Laurian <alexandrumlakar@protonmail.com>
2025-07-07 13:49:33 +0300

    chore(clamav): Update clamav install script to use print_status

--- .pkglist/programs/clamav/install.sh
+++ .pkglist/programs/clamav/install.sh
@@ -3,6 +3,7 @@
 # shellcheck source=/dev/null
 source "$SHELL_COMMONS/commands.sh"
 source "$SHELL_COMMONS/permissions.sh"
+source "$SHELL_COMMONS/strings.sh"
 
 check_root
 check_command "paru"
@@ -12,37 +13,38 @@ CLAMAV_ENTRIES="$PROGRAMS/clamav/entries"
 CLAMAV_SCRIPTS="$PROGRAMS/clamav/scripts"
 CLAMAV_SERVICES="$PROGRAMS/clamav/services"
 
-echo "🔧 Installing ClamAV and required tools..."
+print_status info "Installing ClamAV and required tools..."
 paru -S --skipreview --noconfirm clamav
 "$SUDO" -u "$SUDO_USER" -i paru -S --needed --skipreview --noconfirm clamav-unofficial-sigs
 
 # Copy configs
-echo "🔧 Copying configurations..."
+print_status info "Copying configurations..."
 chown root:root "$CLAMAV_CONFIGS/clamd.conf" && cp -f ./configs/clamd.conf /etc/clamav/clamd.conf
 chown root:root "$CLAMAV_CONFIGS/freshclam.conf" && cp -f ./configs/freshclam.conf /etc/clamav/freshclam.conf
 cp -f "$CLAMAV_CONFIGS/user.conf" /etc/clamav-unofficial-sigs/user.conf
 
 # Copy entries
-echo "🔧 Copying entries..."
+print_status info "Copying entries..."
 cp -f "$CLAMAV_ENTRIES/clamav.desktop" "/usr/share/applications"
 
 # Copy services
-echo "🔧 Copying services..."
-chown root:root "$CLAMAV_SERVICES/clamav-clamonacc.service" && cp -f "$CLAMAV_SERVICES/clamav-clamonacc.service" /usr/lib/systemd/system/clamav-clamonacc.service
+print_status info "Copying services..."
+chown root:root "$CLAMAV_SERVICES/clamav-clamonacc.service"
+cp -f "$CLAMAV_SERVICES/clamav-clamonacc.service" /usr/lib/systemd/system/clamav-clamonacc.service
 
 # Copy scripts
 chmod +x "$CLAMAV_SCRIPTS/virus-event.sh" && cp -f "$CLAMAV_SCRIPTS/virus-event.sh" /etc/clamav/virus-event.sh
 
 # Update virus database
-echo "🔧 Updating virus definitions..."
+print_status info "Updating virus definitions..."
 freshclam
 
-echo "🔧 Reloading systemd daemon..."
+print_status info "Reloading systemd daemon..."
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 
 # Enable services
-echo "🔧 Enabling services..."
+print_status info "Enabling services..."
 systemctl disable clamav-daemon.socket
 systemctl disable clamav-daemon.service
 systemctl disable clamav-freshclam.service
@@ -54,8 +56,8 @@ systemctl enable --now clamav-freshclam.service
 systemctl enable --now clamav-clamonacc.service
 systemctl enable --now clamav-unofficial-sigs.timer
 
-echo "🔧 Reloading systemd daemon..."
+print_status info "Reloading systemd daemon..."
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
 
-echo "✅ ClamAV installed successfully."
+print_status success "ClamAV installed successfully."
