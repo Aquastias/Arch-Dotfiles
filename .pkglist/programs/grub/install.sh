ba965fbcdd4957182981c2bded6c68b60034a4fb
Mlakar Alexandru Laurian <alexandrumlakar@protonmail.com>
2025-07-07 13:31:31 +0300

    chore(grub): Update install script to use print_status

--- .pkglist/programs/grub/install.sh
+++ .pkglist/programs/grub/install.sh
@@ -3,11 +3,12 @@
 # shellcheck source=/dev/null
 source "$SHELL_COMMONS/commands.sh"
 source "$SHELL_COMMONS/permissions.sh"
+source "$SHELL_COMMONS/strings.sh"
 
 check_root
 check_command "paru"
 
-echo "==> Installing grub and required tools..."
+print_status info "Installing grub and required tools..."
 paru -S --skipreview --noconfirm grub os-prober
 
 if [[ -f "$GRUB_DEFAULT_FILE" ]]; then
@@ -19,9 +20,9 @@ if [[ -f "$GRUB_DEFAULT_FILE" ]]; then
     echo 'GRUB_DISABLE_OS_PROBER=false' >>"$GRUB_DEFAULT_FILE"
   fi
 
-  echo "✅ os-prober enabled in $GRUB_DEFAULT_FILE"
+  print_status success "os-prober enabled in $GRUB_DEFAULT_FILE"
 else
-  echo "❌ $GRUB_DEFAULT_FILE not found"
+  print_status error "$GRUB_DEFAULT_FILE not found"
   exit 1
 fi
 
