556b35eb000c6c680c3b4c03dce34ae3284821c0
Mlakar Alexandru Laurian <alexandrumlakar@protonmail.com>
2026-05-15 00:00:34 +0300

    feat(secrets): Deploy SSH identity from user secrets in create-user

--- .os/lib/chroot/create-user.sh
+++ .os/lib/chroot/create-user.sh
@@ -41,6 +41,19 @@ fi
 if [[ -n "$SECRETS_FILE" && -f "$SECRETS_FILE" ]]; then
   sec_pw="$(jq -r '.password // empty' "$SECRETS_FILE")"
   [[ -n "$sec_pw" ]] && PASSWORD="$sec_pw"
+
+  sec_ssh_key="$(jq -r '.ssh_identity_private_key // empty' "$SECRETS_FILE")"
+  if [[ -n "$sec_ssh_key" ]]; then
+    sec_key_type="$(jq -r '.ssh_identity_key_type // "ed25519"' "$SECRETS_FILE")"
+    _ssh_dir="${HOME_BASE:-/home}/$NAME/.ssh"
+    mkdir -p -m 700 "$_ssh_dir"
+    chown "$NAME:$NAME" "$_ssh_dir"
+    printf '%s\n' "$sec_ssh_key" > "$_ssh_dir/id_${sec_key_type}"
+    chmod 600 "$_ssh_dir/id_${sec_key_type}"
+    ssh-keygen -y -f "$_ssh_dir/id_${sec_key_type}" > "$_ssh_dir/id_${sec_key_type}.pub"
+    chmod 644 "$_ssh_dir/id_${sec_key_type}.pub"
+    chown "$NAME:$NAME" "$_ssh_dir/id_${sec_key_type}" "$_ssh_dir/id_${sec_key_type}.pub"
+  fi
 fi
 
 printf '%s:%s\n' "$NAME" "$PASSWORD" | chpasswd
